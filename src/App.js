d742d80cfea503325c3af5da266e0cfd8cbc2358
Eero Nikkinen <eenokki@gmail.com>
2022-03-13 20:21:55 +0200

    Countries from API

---
+++ src/App.js
@@ -0,0 +1,36 @@
+import './App.css';
+import {  useQuery,  gql,} from"@apollo/client";
+
+function App() {
+  const GET_COUNTRIES = gql`
+    query GetCountries {countries{code, name,continent{name}}}`;
+
+const { loading, error, data } = useQuery(GET_COUNTRIES);
+
+if (loading) return<p>Loading...</p>;
+  else if (error) return <p>Error...</p>
+  else {
+    return (
+      <div className="App">
+        <table>
+          <tbody>
+            <tr>
+              <td>Code</td>
+              <td>Name</td>
+              <td>Continent</td>
+            </tr>
+            { data.countries.map((country, index) =>
+            <tr key={index}>
+              <td>{country.code}</td>
+              <td>{country.name}</td>
+              <td>{country.continent.name}</td>
+            </tr>
+            )}
+          </tbody>
+        </table>
+      </div>
+    );
+  }
+}
+
+export default App;
